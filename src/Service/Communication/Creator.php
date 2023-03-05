533ce1499726df30558d8678802375be4d3ef6a8
Yuriy Afanasev <yueafanasev@avito.ru>
2023-03-05 15:38:19 +0100

    video 6.1 example before. step 1

--- src/Service/Communication/Creator.php
+++ src/Service/Communication/Creator.php
@@ -8,27 +8,16 @@ use Service\Communication\Exception\CommunicationException;
 
 class Creator
 {
-    public const TYPE_EMAIL = 'email';
-    public const QUEUE_EMAIL = 'sender_email';
-    public const TYPE_SMS = 'sms';
-    public const QUEUE_SMS = 'sender_sms';
-
     public function sendMessage(string $type): ICommunication
     {
-        return match ($type) {
-            self::TYPE_EMAIL => $this->prepareEmail(),
-            self::TYPE_SMS => $this->prepareSms(),
-            default => throw new CommunicationException('unknown communication type'),
-        };
-    }
+        if ($type === 'email') {
+            return new Email('sender_email');
+        }
 
-    protected function prepareEmail(): ICommunication
-    {
-        return new Email(static::QUEUE_EMAIL);
-    }
+        if ($type === 'sms') {
+            return new Sms('sender_sms');
+        }
 
-    protected function prepareSms(): ICommunication
-    {
-        return new Sms(static::QUEUE_SMS);
+        throw new CommunicationException('unknown communication type');
     }
 }
