2c9826ffe8e398de2d617144b98d03d444799e8f
Yuriy Afanasev <yueafanasev@avito.ru>
2023-03-05 15:43:47 +0100

    video 6.1 example before. step 3

--- src/Service/Communication/Creator.php
+++ src/Service/Communication/Creator.php
@@ -4,31 +4,14 @@ declare(strict_types=1);
 
 namespace Service\Communication;
 
-use Service\Communication\Exception\CommunicationException;
-
-class Creator
+class Creator extends AbstractCreator
 {
-    public const TYPE_EMAIL = 'email';
-    public const QUEUE_EMAIL = 'sender_email';
-    public const TYPE_SMS = 'sms';
-    public const QUEUE_SMS = 'sender_sms';
-
     public function sendMessage(string $type): ICommunication
     {
-        return match ($type) {
-            self::TYPE_EMAIL => $this->prepareEmail(),
-            self::TYPE_SMS => $this->prepareSms(),
-            default => throw new CommunicationException('unknown communication type'),
-        };
-    }
+        if ($type === 'push') {
+            return new Sms('sender_push');
+        }
 
-    protected function prepareEmail(): ICommunication
-    {
-        return new Email(static::QUEUE_EMAIL);
-    }
-
-    protected function prepareSms(): ICommunication
-    {
-        return new Sms(static::QUEUE_SMS);
+        return self::sendMessage($type);
     }
 }
